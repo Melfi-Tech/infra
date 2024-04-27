cde49bfe19c2945257ac03a9d2810e28b39990ef
neutrino2211 <neutrino2211@gmail.com>
2024-04-27 23:08:29 +0100

    Setup tooling

--- setup-server.sh
+++ setup-server.sh
@@ -13,25 +13,4 @@ ufw enable
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 curl -fsSL https://bun.sh/install | bash
 
-exec bash
-
-nvm install 20
-
-npm i ts-node -g
-npm i pm2 -g
-npm i dotenv-cli -g
-
-if [ $? -ne 0 ]; then
-  echo "Return code was not zero but $?, exiting..."
-  exit 1
-fi
-
-# Setup configs
-dotenv -e $1 -- bash ./setup-configs.sh $data_config_dir
-
-# Run docker configs
-
-for i in *compose.yml; do 
-  echo "starting $i"
-  dotenv -e $1 -- docker-compose -f $i up& 
-done
+exec bash setup-tooling.sh
---
+++ setup-tooling.sh
@@ -0,0 +1,21 @@
+# Setup tooling
+nvm install 20
+
+npm i ts-node -g
+npm i pm2 -g
+npm i dotenv-cli -g
+
+if [ $? -ne 0 ]; then
+  echo "Return code was not zero but $?, exiting..."
+  exit 1
+fi
+
+# Setup configs
+dotenv -e $1 -- bash ./setup-configs.sh $data_config_dir
+
+# Run docker configs
+
+for i in *compose.yml; do 
+  echo "starting $i"
+  dotenv -e $1 -- docker-compose -f $i up& 
+done
