03f74d6562d2624ab7cabd26d723191ba678fcb1
neutrino2211 <neutrino2211@gmail.com>
2024-04-27 23:06:03 +0100

    Add check for errors

--- setup-server.sh
+++ setup-server.sh
@@ -21,6 +21,11 @@ npm i ts-node -g
 npm i pm2 -g
 npm i dotenv-cli -g
 
+if [ $? -ne 0 ]; then
+  echo "Return code was not zero but $?, exiting..."
+  exit 1
+fi
+
 # Setup configs
 dotenv -e $1 -- bash ./setup-configs.sh $data_config_dir
 
