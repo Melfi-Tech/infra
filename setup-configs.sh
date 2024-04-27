260e598aceeb48dcf27022d10da2e16bf944d61b
neutrino2211 <neutrino2211@gmail.com>
2024-04-27 22:54:36 +0100

    Server setup scripts

---
+++ setup-configs.sh
@@ -0,0 +1,35 @@
+if [ -z ${2+x} ]; then echo "config destinations not provided" && exit 1; fi
+
+postgres_compose=$1/pg_compose.yml
+
+# Setup postgres data
+cat << EOF >> $postgres_compose
+version: "3.8"
+services:
+  db:
+    image: postgres
+    container_name: local_pgdb
+    restart: always
+    ports:
+      - "5432:5432"
+    environment:
+      POSTGRES_USER: $PG_USER
+      POSTGRES_PASSWORD: $PG_PASS
+    volumes:
+      - local_pgdata:/var/lib/postgresql/data
+  pgadmin:
+    image: dpage/pgadmin4
+    container_name: pgadmin4_container
+    restart: always
+    ports:
+      - "8888:80"
+    environment:
+      PGADMIN_DEFAULT_EMAIL: admin@melfi.com
+      PGADMIN_DEFAULT_PASSWORD: $PG_PASS
+    volumes:
+      - pgadmin-data:/var/lib/pgadmin
+
+volumes:
+  local_pgdata:
+  pgadmin-data:
+EOF
---
+++ setup-server.sh
@@ -0,0 +1,28 @@
+if [ -z ${1+x} ]; then echo "env file not provided" && exit 1; fi
+if [ -z ${2+x} ]; then echo "config destinations not provided" && exit 1; fi
+
+# Create necessary directories
+data_config_dir=$2/config
+
+# Install system dependencies
+apt install ufw docker docker-compose
+ufw allow 22
+ufw enable
+
+# Install NVM & Bun
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+curl -fsSL https://bun.sh/install | bash
+source $HOME/.bashrc
+
+nvm install 20
+
+npm i ts-node -g
+npm i pm2 -g
+npm i dotenv-cli -g
+
+# Setup configs
+dotenv -e $1 -- bash ./setup-configs.sh $data_config_dir
+
+# Run docker configs
+
+for i in *compose.yml; do echo "starting $i" && dotenv -e $1 -- docker-compose -f $i up&; done
