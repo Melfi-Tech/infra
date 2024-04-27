2d60ab81b16840e920f65ef845755cf6bc213131
neutrino2211 <neutrino2211@gmail.com>
2024-04-27 23:13:20 +0100

    Manually load nvm

--- setup-tooling.sh
+++ setup-tooling.sh
@@ -1,5 +1,8 @@
 # Setup tooling
 . /root/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install 20
 
 npm i ts-node -g
